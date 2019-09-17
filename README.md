37abf95cfa23c42f2476e58c54f72d3ca40a6b3f
Simon Cordova <>
2019-09-17 02:36:36 +0100

    Further additions

--- README.md
+++ README.md
@@ -1,5 +1,17 @@
 This is a starter package for those using the MERNG stack.
 
+To run the application, navigate to the top level directory and run:
+yarn run dev
+
 It utilises an ApolloServer with a simple schema and resolver.
 
 The client folder contains a stripped-down version of create-react-app.
+
+Modify the object in the config.js file to connect to your database
+
+Set up a config.js file with the following structure:
+module.exports = {
+MONGODB:
+"mongodb+srv://<username>:<password>@cluster0-lsrec.mongodb.net/test?retryWrites=true&w=majority",
+SECRET_KEY: "some very secret key"
+};
--- index.js
+++ index.js
@@ -1,20 +1,17 @@
 const { ApolloServer, gql } = require("apollo-server");
+const mongoose = require("mongoose");
+
+const { MONGODB } = require("./config.js");
 
 // A schema is a collection of type definitions (hence "typeDefs")
 // that together define the "shape" of queries that are executed against
 // your data.
 const typeDefs = gql`
-  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
-
-  # This "Book" type defines the queryable fields for every book in our data source.
   type Book {
     title: String
     author: String
   }
 
-  # The "Query" type is special: it lists all of the available queries that
-  # clients can execute, along with the return type for each. In this
-  # case, the "books" query returns an array of zero or more Books (defined above).
   type Query {
     books: [Book]
   }
@@ -43,7 +40,12 @@ const resolvers = {
 // definition and your set of resolvers.
 const server = new ApolloServer({ typeDefs, resolvers });
 
-// The `listen` method launches a web server.
-server.listen().then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`);
-});
+mongoose
+  .connect(MONGODB, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("MongoDB Connected");
+    return server.listen({ port: 5000 });
+  })
+  .then(({ url }) => {
+    console.log(`🚀  Server ready at ${url}`);
+  });
